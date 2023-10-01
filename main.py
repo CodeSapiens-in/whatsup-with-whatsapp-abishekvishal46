4262b67b4f5df9e594510bba9cf8a2cd53a94590
abishekvishal46 <146636575+abishekvishal46@users.noreply.github.com>
2023-10-01 18:24:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,20 @@ with open(r"_chat.txt",  encoding="utf8") as fp:
             pollcount=pollcount+1
     print('Total Number of lines:', lines)
     print('Total polls: '+str(pollcount))
+    participant_names = set()
+
+for message in chat_messages:
+    if "joined using" in message:
+
+        parts = message.split('~')
+        if len(parts) > 1:
+            participant_name = parts[1]
+            participant_names.add(participant_name)
+
+        # Debug prints
+
+print(participant_names)
+total_participants = len(participant_names)
+print("Total number of participants:", total_participants)
+
     
